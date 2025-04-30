d3387c31ff18cee04d3b40ab83ad2d03ca05cb1a
Matheus Bailo <matheusserrute@gmail.com>
2025-04-30 16:51:03 -0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+
+
+
+export default function Home() {
+  return (
+   <>
+   
+      <h1 className="text-black justify-center flex items-center">Turminha bacana</h1>
+        <div className="justify-center flex items-center">
+          <h2 className="text-red-600 ">Teste</h2>
+        </div>
+        <div className="justify-center flex items-center mt-20 p-5 rounded-2xl border-4 border-orange-600 pg w-70 ml-200">
+            <div className="">
+                  <h2 className="justify-center flex items-center">Teste card</h2>
+                  <p className="justify-center flex items-center" >aboboraaaaaaaaaaaa</p>
+            </div>
+        </div>
+
+   </>
+  );
+}
